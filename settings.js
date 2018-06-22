b8adc5c489d5cb88690d219dd4393e5a93643255
PaddyCube <paddy-daun@web.de>
2018-06-22 12:11:51 +0200

    Cleanup, Manual control
    Added manual control and clean up stuff
    
    WARNING: MANUAL CONTROL IS EXPERIMENTAL!!

--- settings.js
+++ settings.js
@@ -61,7 +61,7 @@ module.exports = {
     //debugUseColors: true,
 
     // The file containing the flows. If not set, it defaults to flows_<hostname>.json
-    //flowFile: 'flows.json',
+    flowFile: 'flows_ardumower.json',
 
     // To enabled pretty-printing of the flow within the flow file, set the following
     //  property to true:
